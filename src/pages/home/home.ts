b5bbf3437057339e01ec2045e8c14cb3c06ed72f
Vinicius Tonon <vinicius@Viniciuss-MacBook-Pro.local>
2018-09-30 23:18:38 -0300

    Puta merda, que coisa feia de se ver

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+      console.log('AMENO')
+
   }
 
+    playStream(streamName):void {
+        var audio : HTMLMediaElement = <HTMLAudioElement> document.getElementById(streamName)
+        console.log(audio)
+        console.log('slk')
+        audio.play();
+    }
 }
